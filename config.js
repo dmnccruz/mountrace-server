57563a1dc2f6b7c852b393149aa94c78883c3f37
dominicmartincruz <dominicmartincruz@gmail.com>
2020-11-24 17:05:59 +0800

    test

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://admin:uOZu7tl5Gd1CunMX@coronasave.wmr2b.mongodb.net/coronasave?retryWrites=true&w=majority',
+    SECRET_KEY: 'some very secret key'
+}
+
